5398f46b379d2d48f6cbe6068b2acf5d8df5af4e
Aaron Powell <Apowell829@gmail.com>
2019-01-15 22:26:03 +0000

    Refactoring

---
+++ app.js
@@ -0,0 +1,13 @@
+const game1 = new Hangman("cat", 3);
+const guessesEl = document.querySelector("#guesses");
+const puzzleEl = document.querySelector("#puzzle");
+
+puzzleEl.textContent = `Word: ${game1.getPuzzle()}`;
+guessesEl.textContent = `Guesses: ${game1.guesses}`;
+
+window.addEventListener("keypress", function(e) {
+  const guess = String.fromCharCode(e.charCode);
+  game1.guess(guess);
+  puzzleEl.textContent = `Word: ${game1.getPuzzle()}`;
+  guessesEl.textContent = `Guesses: ${game1.guesses}`;
+});
--- hangman.js
+++ hangman.js
@@ -26,17 +26,3 @@ Hangman.prototype.guess = function(guess) {
     this.guesses--;
   }
 };
-
-const game1 = new Hangman("cat", 3);
-const guessesEl = document.querySelector("#guesses");
-const puzzleEl = document.querySelector("#puzzle");
-
-puzzleEl.textContent = `Word: ${game1.getPuzzle()}`;
-guessesEl.textContent = `Guesses: ${game1.guesses}`;
-
-window.addEventListener("keypress", function(e) {
-  const guess = String.fromCharCode(e.charCode);
-  game1.guess(guess);
-  puzzleEl.textContent = `Word: ${game1.getPuzzle()}`;
-  guessesEl.textContent = `Guesses: ${game1.guesses}`;
-});
