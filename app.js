d10f3c841e09453e568ff90f3f5efca1ff13db10
Aaron Powell <Apowell829@gmail.com>
2019-01-17 18:23:00 +0000

    HTTP Request experimenting

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ window.addEventListener("keypress", e => {
   puzzleEl.textContent = `Word: ${game1.puzzle}`;
   guessesEl.textContent = game1.message;
 });
+
+const request = new XMLHttpRequest();
+
+request.addEventListener("readystatechange", e => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText);
+    console.log(data);
+    const country = data.find(country => country.alpha2Code === "GB");
+    console.log(country.name);
+  } else if (e.target.readyState === 4) console.log("Unable to fetch data");
+});
+
+request.open("GET", "http://restcountries.eu/rest/v2/all");
+request.send();
