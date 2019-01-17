c6da91863377e23e4210bc99e69262bed824e00b
Aaron Powell <Apowell829@gmail.com>
2019-01-17 17:31:14 +0000

    ES6 Arrow function change

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const game1 = new Hangman("cat", 2);
 const guessesEl = document.querySelector("#guesses");
 const puzzleEl = document.querySelector("#puzzle");
 
-puzzleEl.textContent = `Word: ${game1.getPuzzle()}`;
-guessesEl.textContent = game1.getMessage();
+puzzleEl.textContent = `Word: ${game1.puzzle}`;
+guessesEl.textContent = game1.message;
 
-window.addEventListener("keypress", function(e) {
+window.addEventListener("keypress", e => {
   const guess = String.fromCharCode(e.charCode);
   game1.guess(guess);
-  puzzleEl.textContent = `Word: ${game1.getPuzzle()}`;
-  guessesEl.textContent = game1.getMessage();
+  puzzleEl.textContent = `Word: ${game1.puzzle}`;
+  guessesEl.textContent = game1.message;
 });
