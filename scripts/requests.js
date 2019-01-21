da2c5998ebb0b32e1fa07bbdca7570c0210d496b
Aaron Powell <Apowell829@gmail.com>
2019-01-21 00:08:39 +0000

    changed http to be detected on server

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,6 +1,6 @@
 const getPuzzle = async wordCount => {
   const response = await fetch(
-    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
+    `//puzzle.mead.io/puzzle?wordCount=${wordCount}`
   );
   if (response.status === 200) {
     const data = await response.json();
@@ -10,29 +10,29 @@ const getPuzzle = async wordCount => {
   }
 };
 
-const getCountryDetails = async cCode => {
-  const response = await fetch("http://restcountries.eu/rest/v2/all");
-  if (response.status === 200) {
-    const data = await response.json();
-    return data.find(country => country.alpha2Code === `${cCode}`);
-  } else {
-    throw new Error(`Eorrororor`);
-  }
-};
+// const getCountryDetails = async cCode => {
+//   const response = await fetch("//restcountries.eu/rest/v2/all");
+//   if (response.status === 200) {
+//     const data = await response.json();
+//     return data.find(country => country.alpha2Code === `${cCode}`);
+//   } else {
+//     throw new Error(`Eorrororor`);
+//   }
+// };
 
 // http://ipinfo.io/json?token=1a11bd55cc8f9c
 
-const getLocation = async () => {
-  const response = await fetch("http://ipinfo.io/json?token=1a11bd55cc8f9c");
-  if (response.status === 200) {
-    return await response.json();
-  } else {
-    throw new Error(`Lol`);
-  }
-};
+// const getLocation = async () => {
+//   const response = await fetch("//ipinfo.io/json?token=1a11bd55cc8f9c");
+//   if (response.status === 200) {
+//     return await response.json();
+//   } else {
+//     throw new Error(`Lol`);
+//   }
+// };
 
-const getCurrentCountry = async () => {
-  const location = await getLocation();
-  return getCountryDetails(location.country);
-  //Same as storing await function and returning variable
-};
+// const getCurrentCountry = async () => {
+//   const location = await getLocation();
+//   return getCountryDetails(location.country);
+//   //Same as storing await function and returning variable
+// };
